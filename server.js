97a7d1c4aefad86e73da22e575a3f4d5ddaf2986
YasinYA <yaasiinyuusuf78@gmail.com>
2016-04-13 04:29:21 +0800

    added the Procfile for heroku deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express'),
 	bodyParser = require('body-parser');
 
 app
+	.set('port', process.env.PORT || 3000)
 	.use(express.static('./public/'))
 	.use(bodyParser.json())
 	.get('/data', function(req, res) {
@@ -11,5 +12,5 @@ app
 	.get('*', function(req, res) {
 		res.sendfile('public/index.html');
 	})
-	.listen(3000);
-	console.log('Server is running on port 3000');
+	.listen(app.get('port'), function(){  console.log('Server is running on port ' + app.get('port')); });
+	
